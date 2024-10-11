77d0c6e829745b3e728902481bc36f5994030efd
sreypheaphorn <sreyphea.phorn@institute.pse.ngo>
2024-10-11 09:21:40 +0700

    Back end portfoilo

---
+++ src/api/sreyphea/controllers/sreyphea.ts
@@ -0,0 +1,7 @@
+/**
+ * sreyphea controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sreyphea.sreyphea');
---
+++ src/api/sreyphea/routes/sreyphea.ts
@@ -0,0 +1,7 @@
+/**
+ * sreyphea router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::sreyphea.sreyphea');
